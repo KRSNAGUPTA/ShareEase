26a3d003cb5c04e83ce8f594e479793b7238f26e
KRSNAGUPTA <up.krishnagupta@gmail.com>
2024-10-16 22:50:04 +0530

    Refactor server.js and imageController.js
    - Refactor server.js to add a welcome message for the backend API and include a new route for image uploads.
    - Add imageController.js to handle image upload functionality using the Imgur API.
    - Implement multerMiddleware.js to handle file uploads and set file size limits and supported file types.

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import imageRoute  from "./src/routes/imageRoute.js";
 dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+const corsOrigin = process.env.CORS_ORIGIN || "*";
 
 const app = express();
 app.use(express.json());
-const corsOrigin = process.env.CORS_ORIGIN || "*";
 app.use(cors({ origin: corsOrigin }));
-const PORT = process.env.PORT || 5000;
+
+app.get("/api", (req, res) => {
+    res.send("Backend mai aapka swagat hai!");
+});
+app.use('/api/image', imageRoute);
+
 app.listen(PORT, (err) => {
   if (err) {
     console.error("Failed to start server:", err);
