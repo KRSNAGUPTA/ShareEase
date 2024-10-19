1f5596f827d7e863f2522cdf6dbebb0c71664936
KRSNAGUPTA <up.krishnagupta@gmail.com>
2024-10-19 23:02:40 +0530

    Refactor routing functionality and add slug-based not found page
    - Refactor App.jsx to use react-router-dom for routing functionality
    - Add SluggedNotFound component to handle slug-based not found page
    - Create NotFound.jsx component for displaying a generic not found page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
+import { useParams } from 'react-router-dom';  
+import './App.css';
+
+const NotFound = lazy(() => import('./components/NotFound.jsx'));
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div className="">Hello</div>
+      <Router>
+        <Suspense fallback={<h1>Loading...</h1>}>
+          <Routes>
+            <Route path="/:slug" element={<SluggedNotFound />} /> {/* for testing only , slug find krne ke liye*/} 
+          </Routes>
+        </Suspense>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+function SluggedNotFound() {
+  const { slug } = useParams(); 
+  return <NotFound slug={slug} />;
+}
+
+export default App;
---
+++ frontend/src/components/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotFound = ({slug = "kuch nhi aya"}) => {
+  return (
+    <div>
+      <h1>Hello {slug}</h1>
+    </div>
+  );
+};
+
+export default NotFound;
