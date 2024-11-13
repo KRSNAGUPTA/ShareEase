c299a6dbb0883004f79c810e41661fee530d6775
Aditya Kirti <addyyy118@gmail.com>
2024-11-13 14:04:27 +0530

    Add Shorten Link page

--- frontend/src/components/LinkProvider.jsx
+++ frontend/src/components/LinkProvider.jsx
@@ -1,8 +1,9 @@
+/* eslint-disable react/prop-types */
 import copyIcon from "../assets/copy.svg"
 
 export default function LinkProvider({ link }) {
     return (
-        <div className="p-0 inline-flex items-center justify-center border shadow-lg">
+        <div className="p-0 inline-flex items-center justify-center border shadow-lg rounded">
             <div className="py-4 px-8 ">
                 {link}
             </div>
