cd5f9ab0dd5e30968e16530f93611a837a29e5fb
KRSNAGUPTA <up.krishnagupta@gmail.com>
2024-11-17 01:11:02 +0530

    Refactor HomePage component for improved layout and responsiveness; adjust spacing and structure

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,21 +3,27 @@ import Demo from "../components/Demo";
 import { Intro, Video } from "../components/Demo";
 
 function HomePage() {
-    return (
-        <>
-            <NavBar />
-            <div className="flex flex-col gap-20 pb-20">
-                <Demo>
-                    <Intro to="/share" button="Share">Share files in almost no time!</Intro>
-                    <Video></Video>
-                </Demo>
-                <Demo>
-                    <Video></Video>
-                    <Intro to="/shorten" button="Shorten">Make your links easy to memorize!</Intro>
-                </Demo>
-            </div>7
-        </>
-    )
+  return (
+    <>
+      <div className="w-full h-screen">
+        <NavBar />
+        <div className="flex flex-col gap-12">
+          <Demo className="flex sm:flex-wrap">
+            <Intro to="/share" button="Share">
+              Share files in almost no time!
+            </Intro>
+            <Video></Video>
+          </Demo>
+          <Demo>
+            <Video></Video>
+            <Intro to="/shorten" button="Shorten">
+              Make your links easy to memorize!
+            </Intro>
+          </Demo>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default HomePage;
